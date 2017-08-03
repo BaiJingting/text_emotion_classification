3a3f74f9c180e01296cad1958b3167f8d13575b8
Bai <baijt@ruc.edu.cn>
2017-08-03 21:14:26 +0800

    initional version

---
+++ apply_preprogress.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from multiprocessing import Pool
+
+ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(ROOT_PATH)
+
+from src.datafactory.weibo_emotion_classification import constant
+from src.datafactory.weibo_emotion_classification import segment_process
+from src.datafactory.weibo_emotion_classification import tb_weibo_content_for_classification as twc
+
+
+def get_apply_dataset(startdate, enddate):
+    """
+    :param startdate:
+    :param enddate:
+    :return:
+    """
+    result = __read_weibo_data(startdate, enddate)
+    data = __get_segment(result)
+
+    data_x = []
+    data_y = []
+    for line in data:
+        if line.successful() and line.get() is not None:
+            data_x.append(line.get()['content_detail'])
+            data_y.append(line.get()['emotion_type'])
+    return data_x, data_y
+
+
+def __read_weibo_data(start_date, end_date, data_source_ids=1):
+    """
+    :param start_date:
+    :param end_date:
+    :param data_source_ids:
+    :return:
+    """
+    my_filter = twc.Filter()
+    my_filter.set_content_datetime(start_date, end_date)
+    my_filter.set_data_source_ids(data_source_ids)
+    query = twc.query(my_filter)
+    print "total number of data: %d" % len(query)
+    return query
+
+
+def __get_segment(result):
+    """
+    :param result:
+    :return:
+    """
+    ret = []
+    pool = Pool(processes=constant.process_num)
+    try:
+        for line in result:
+            seg_res = pool.apply_async(__single_segment, args=(line,))
+            ret.append(seg_res)
+        pool.close()
+        pool.join()
+    except KeyboardInterrupt:
+        print "Caught KeyboardInterrupt, terminating workers"
+        pool.terminate()
+        pool.join()
+    return ret
+
+
+# ## 过滤部分词性
+# def single_segment(line):
+#     aux = line['content_detail'].replace("\\", "\\\\").replace("\"", "\\\"") \
+#         .replace("\'", "\\\"").replace("%", "").replace("`", "")
+#     aux = postag_process.get_postag(aux)
+#     if len(aux) == 0:
+#         return
+#     line['content_detail'] = filter_postag(aux)
+#     return line
+
+
+def __single_segment(line):
+    """
+    :param line:
+    :return:
+    """
+    aux = line['content_detail'].replace("\\", "\\\\").replace("\"", "\\\"") \
+        .replace("\'", "\\\"").replace("%", "").replace("`", "")
+    line['content_detail'] = segment_process.get_segment(aux)
+    if len(aux) == 0:
+        return
+    return line
+
+
+def filter_postag(list):
+    """
+    :param list:
+    :return:
+    """
+    ret = []
+    for i in list:
+        if i[1] in constant.KEEP_POS:
+            ret.append(i[0])
+    return ret
+
+
+def lda_vecs(dictionary, lda_model, data):
+    """
+    :param dictionary:
+    :param lda_model:
+    :param data:
+    :return:
+    """
+    ret = []
+    for line in data:
+        line = dictionary.doc2bow(line)
+        arr = [0 for i in range(constant.topic_nums)]
+        for item in lda_model[line]:
+            arr[item[0]] = item[1]
+        ret.append(arr)
+    return ret
---
+++ main.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import pickle
+from gensim import models
+from gensim.corpora import Dictionary
+from sklearn.model_selection import train_test_split
+
+ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(ROOT_PATH)
+
+from src.datafactory.weibo_emotion_classification import apply_preprogress as ap
+import src.datafactory.weibo_emotion_classification.classification_model as cm
+from src.datafactory.weibo_emotion_classification import constant
+import src.datafactory.weibo_emotion_classification.train_preprogress as tp
+import src.datafactory.weibo_emotion_classification.model_evaluation as me
+
+if __name__ == "__main__":
+
+    if constant.OBJECTIVE == "train_overall":
+
+        data_x, data_y = tp.get_train_dataset(constant.POS_START_DATE, constant.NEG_START_DATE,
+                                              constant.NEU_START_DATE, constant.TRAIN_END_DATE)
+
+        ## lda
+        dictionary = Dictionary(data_x)
+        dictionary.filter_extremes(no_below=5, no_above=0.7)
+        dictionary.save(constant.dictionary_path)
+
+        corpus = [dictionary.doc2bow(text) for text in data_x]
+        lda_model = models.LdaModel(corpus, id2word=dictionary,
+                                    num_topics=constant.topic_nums, iterations=500)
+        lda_model.save(constant.lda_model_path)
+        data_x = tp.lda_vecs(lda_model, corpus)
+
+        ### classify
+
+        x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.2)
+
+        model = cm.train(x_train, y_train, constant.classification_model_path)
+        pred_y = cm.predict(model, x_test)
+        me.classification_evaluate(y_test, pred_y)
+
+    elif constant.OBJECTIVE in ["apply_reclassify", "train_reclassify"]:
+        if constant.OBJECTIVE == "train_reclassify":
+            data_x, data_y = ap.get_apply_dataset(constant.RECLASSIFY_TRAIN_START_DATE,
+                                                  constant.RECLASSIFY_TRAIN_END_DATE)
+        else:
+            data_x, data_y = ap.get_apply_dataset(constant.RECLASSIFY_APPLY_START_DATE,
+                                                  constant.RECLASSIFY_APPLY_END_DATE)
+
+        lda_model = models.LdaModel.load(constant.lda_model_path)
+        dictionary = Dictionary.load(constant.dictionary_path)
+        x_vecs = ap.lda_vecs(dictionary, lda_model, data_x)
+
+        with open(constant.classification_model_path, "r") as f:
+            classification_model = pickle.load(f)
+
+        pred_y = cm.predict(classification_model, x_vecs)
+
+        me.classification_evaluate(data_y, pred_y)
+
+        ## 在召回基础上提升精确率
+        if constant.OBJECTIVE == "train_reclassify":
+
+            pos_x, pos_y, neg_x, neg_y = tp.reclassify_data(x_vecs, data_y, pred_y)
+            tp.retrain(pos_x, pos_y, neg_x, neg_y)
+
+        else:
+
+            pos_x, pos_y, neg_x, neg_y = tp.reclassify_data(x_vecs, data_y, pred_y)
+
+            with open(constant.reclassify_pos_model_path, "r") as f:
+                reclassify_pos_model = pickle.load(f)
+            pos_pred_y = reclassify_pos_model.predict(pos_x)
+            print "reclassify_pos ---------------------------------------------"
+            me.classification_evaluate(pos_y, pos_pred_y)
+
+            with open(constant.reclassify_neg_model_path, "r") as f:
+                reclassify_neg_model = pickle.load(f)
+            neg_pred_y = reclassify_neg_model.predict(neg_x)
+            print "reclassify_neg ---------------------------------------------"
+            me.classification_evaluate(neg_y, neg_pred_y)
+
+            ## 综合评估
+            me.evaluate(data_y, pred_y, pos_pred_y, neg_pred_y)
+
+
+    elif constant.OBJECTIVE == "apply_overall":
+
+        data_x, data_y = ap.get_apply_dataset(constant.APPLY_START_DATE, constant.APPLY_END_DATE)
+
+        lda_model = models.LdaModel.load(constant.lda_model_path)
+        dictionary = Dictionary.load(constant.dictionary_path)
+        x_vecs = ap.lda_vecs(dictionary, lda_model, data_x)
+
+        with open(constant.classification_model_path, "rb") as f:
+            classification_model = pickle.load(f)
+
+        pred_y = cm.predict(classification_model, x_vecs)
+        cm.classification_evaluate(data_y, pred_y)
